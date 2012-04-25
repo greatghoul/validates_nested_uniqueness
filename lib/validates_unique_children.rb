dd07de83b648f6cf8e00d49601a969c06f1b25df
Kyle Stevens <kstevens715@gmail.com>
2012-04-25 16:53:51 -0400

    Added initializer

--- lib/validates_unique_children.rb
+++ lib/validates_unique_children.rb
@@ -1,5 +1,17 @@
 require "validates_unique_children/version"
+require 'active_model'
 
-module ValidatesUniqueChildren
-  # Your code goes here...
+module ActiveModel
+  module Validations
+    class ValidatesUniqueChildrenValidator < ActiveModel::EachValidator
+      
+      def initialize(options)
+      end
+
+      def validate_each(record, attribute, value)
+        record.errors[attribute] << "wow it works"
+      end
+
+    end
+  end
 end
